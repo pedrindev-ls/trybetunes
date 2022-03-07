27eb0dd3bcc31ac6737b32fb168a49ceb4443654
Pedrin <pedro515vieira@gmail.com>
2022-03-07 20:13:25 -0300

    requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 
 class Header extends Component {
@@ -22,6 +23,17 @@ class Header extends Component {
     const { user } = this.state;
     return (
       <header data-testid="header-component">
+        <Link to="/search" data-testid="link-to-search">
+          <button type="button">
+            Pesquisar
+          </button>
+        </Link>
+        <Link to="/favorites" data-testid="link-to-favorites">
+          <button type="button">Favoritos</button>
+        </Link>
+        <Link to="/profile" data-testid="link-to-profile">
+          <button type="button">Perfil</button>
+        </Link>
         {!user.name ? 'Carregando...' : <p data-testid="header-user-name">{user.name}</p>}
       </header>
     );
