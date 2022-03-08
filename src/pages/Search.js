6085cc9376fe42273ebcebf360cd78b0c49b392b
Pedrin <pedro515vieira@gmail.com>
2022-03-08 16:14:08 -0300

    Requisito 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,23 +1,45 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends Component {
   constructor() {
     super();
     this.state = {
-      artist: '',
+      search: '',
       button: true,
+      loading: false,
+      albunsFound: [],
+      artistFound: false,
+      artistFoundName: '',
     };
   }
 
   handleChange = ({ target }) => {
-    const { value } = target;
-    this.setState({ artist: value }, this.validateButton);
+    const { value, name } = target;
+    this.setState({ [name]: value }, this.validateButton);
+  }
+
+  confirmedSearch = async () => {
+    const { search } = this.state;
+    const searched = search;
+    this.setState({
+      loading: true,
+      search: '',
+      artistFoundName: searched,
+    });
+    const albuns = await searchAlbumsAPI(searched);
+    this.setState({
+      loading: false,
+      albunsFound: albuns,
+      artistFound: true,
+    });
   }
 
   validateButton = () => {
-    const { artist } = this.state;
-    if (artist.length >= 2) {
+    const { search } = this.state;
+    if (search.length >= 2) {
       this.setState({ button: false });
     } else {
       this.setState({ button: true });
@@ -25,30 +47,64 @@ class Search extends Component {
   }
 
   render() {
-    const { button } = this.state;
+    const {
+      button, search, loading, albunsFound, artistFound, artistFoundName } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <section>
-          <form>
-            <label htmlFor="search">
-              <input
-                type="text"
-                id="search"
-                data-testid="search-artist-input"
-                onChange={ this.handleChange }
-              />
-            </label>
-            <button
-              type="button"
-              data-testid="search-artist-button"
-              disabled={ button }
-            >
-              Procurar
+        {loading ? <p>Carregando...</p>
+          : (
+            <section>
+              <form>
+                <label htmlFor="search">
+                  <input
+                    type="text"
+                    id="search"
+                    name="search"
+                    value={ search }
+                    data-testid="search-artist-input"
+                    onChange={ this.handleChange }
+                  />
+                </label>
+                <button
+                  type="button"
+                  data-testid="search-artist-button"
+                  disabled={ button }
+                  onClick={ this.confirmedSearch }
+                >
+                  Procurar
 
-            </button>
-          </form>
-        </section>
+                </button>
+              </form>
+            </section>)}
+        <hr />
+        <div>
+          {albunsFound.length > 0 && artistFound
+            ? (
+              <p>
+                Resultado de álbuns de:
+                {' '}
+                {artistFoundName}
+              </p>)
+            : ''}
+          {albunsFound.length === 0 && artistFound
+            ? <p>Nenhum álbum foi encontrado</p>
+            : albunsFound.map((album) => (
+              <Link
+                key={ album.collectionId }
+                data-testid={ `link-to-album-${album.collectionId}` }
+                to={ `/album/${album.collectionId}` }
+              >
+                <div key={ album.collectionId }>
+                  <img
+                    src={ album.artworkUrl100 }
+                    alt={ `Capa do Album ${album.collectionName}` }
+                  />
+                  <p>{ album.collectionName }</p>
+                  <p>{ album.artistName }</p>
+                </div>
+              </Link>))}
+        </div>
       </div>
     );
   }
